1b6542234332846fd11fb66e2b24398fbd902103
JafarovAA <djafarov_a@mineframe.ru>
2025-09-25 17:02:47 +0500

    Обновлены пути к статическим файлам в шаблонах и настройках. Добавлен `@charset "UTF-8";` в `bootstrap.min.css`. Исправлены ссылки на иконки и флаги, теперь они загружаются из статических файлов. Изменен `STATIC_URL` на '/static/' в `settings.py` для корректной работы с статическими ресурсами.

--- vl09_web/settings.py
+++ vl09_web/settings.py
@@ -148,7 +148,7 @@ GUNICORN_TIMEOUT = 300
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
